cd1a09b5317f39846f8fec57e843b005b74ff4f2
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2024-09-11 00:55:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (99.30%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -13,22 +13,33 @@ public:
             word+=s[i];
             else
             {
+                // Got a word
+                // Pattern[X] present in the map ???
                 if(m.find(pattern[x])!=m.end())
                 {
+                    // Present in the map
+                    // m[pattern[x]] has a different value than the word which is incoming
                     if(m[pattern[x]]!=word)
                     return false;
                 } 
+
+                // Pattern[X] not present in the map
                 else
                 {
+                    // is the incoming word linked to any other pattern ?? If yes then we return false
                     if(res.find(word)!=res.end())
                     return false;
+                    //new pattern and new word incoming
+                    //insert into the set and map
                     res.insert(word);
                     m[pattern[x]]=word;
                 }
+                //empty the word and increase x for new incoming data
                 word="";
                 x++;
             }
         }
+        //x and pattern's length matches
         if(pattern.length()==x)
         return true;
         else
