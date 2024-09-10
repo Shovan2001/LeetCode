a30f00ad54241d691f2fa76b87e251f4748c6882
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2024-09-10 20:41:58 +0530

    Time: 7 ms (34.82%) | Memory: 23 MB (29.31%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -18,18 +18,8 @@ public:
             //add spaces in between words // We know it was a word when s[i-1]!=' ' (prev letter is not a space)
             if(s[i]==' ' && s[i-1]!=' ')
             str+=s[i];
-            // // //remove leading spaces
-            // // if(s[i]==' ' && str.length()==0)
-            // // continue;
-            // //enter a space in between words as s[i-1]!=' '
-            // if(s[i]==' ' && str.length()!=0 && s[i-1]!=' ')
-            // str+=s[i];
-            // //igores all rest spaces
-            // if(s[i]!=' ')
-            // str+=s[i];
         }
         str+=' ';
-        // cout<<str;
         
         string word="",rev="";
         for(int i=0;i<str.length();i++)
@@ -44,44 +34,5 @@ public:
             }
         }
         return rev.substr(0,rev.length()-1);
-        // for(int i=0;i<str.length();i++)
-        // {
-        //     if(str[i]!=' ')
-        //     word+=s[i];
-        //     else
-
-        // }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // string res="",rev="";
-        // for(int i=0;i<str.length();i++)
-        // {
-        //     if(str[i]!=' ')
-        //     res+=str[i];
-        //     if(str[i]==' ' && str[i-1]!=' ')
-        //     res+=" ";
-        //     if(str[i]==' ')
-        //     {
-        //         rev=res+rev;
-        //         res="";
-        //     }
-            
-        // }
-        // return rev.substr(0,rev.length()-1);
-        // return s;
     }
 };
