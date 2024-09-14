0f8a8a9704db3708e2e856f3d68bc9ab95e00be9
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2024-09-14 21:40:23 +0530

    Time: 110 ms (5.62%) | Memory: 87.9 MB (5.11%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    // void helper(vector<vector<int>>&ans,vector<int> &subans,vector<int> &candidates,int index,int target)
+    // {
+    //     if(index==candidates.size())
+    //     {
+    //         if(target==0)
+    //             ans.push_back(subans);
+    //         return;
+    //     }
+        
+    //     if(candidates[index]<=target){
+    //     subans.push_back(candidates[index]);
+    //     //idx not changed to idx+1 as same number can be chosen multiple times
+    //     helper(ans,subans,candidates,index,target-candidates[index]);
+    //     subans.pop_back();
+    //     }
+        
+    //     helper(ans,subans,candidates,index+1,target);
+        
+    // }
+    void solve(vector<vector<int>>&ans,vector<int>&res,vector<int>nums,int idx,int target)
+    {
+        if(idx==nums.size())
+        {
+            if(target==0)
+                ans.push_back(res);
+            return;
+        }
+        vector<int>op1=res;
+        vector<int>op2=res;
+
+        if(nums[idx]<=target)
+        {
+            op1.push_back(nums[idx]);
+            solve(ans,op1,nums,idx,target-nums[idx]);
+        }
+        solve(ans,op2,nums,idx+1,target);
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+    {
+        vector<vector<int>>ans;
+        vector<int>res;
+        solve(ans,res,candidates,0,target);
+        return ans;
+
+        // vector<vector<int>>ans;
+        // vector<int>subans;
+        // helper(ans,subans,candidates,0,target);
+        // return ans;
+        
+    }
+};
