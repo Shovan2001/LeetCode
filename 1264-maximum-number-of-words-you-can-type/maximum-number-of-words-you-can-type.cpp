63f0c396dc73659d017ee5b37e944c8f72a2e770
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2024-11-04 20:16:11 +0530

    Time: 12 ms (5.63%) | Memory: 8 MB (88.13%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) 
+    {
+        string word="";
+        int x=0;
+        text+=' ';
+        for(int i=0;i<text.length();i++)
+        {
+            if(text[i]!=' ')
+            word+=text[i];
+            else
+            {
+                int c=0;
+                cout<<word<<"\n";
+                for(int j=0;j<brokenLetters.length();j++)
+                {
+                    if(word.find(brokenLetters[j])!=string::npos)
+                    {
+                        c=1;
+                        break;
+                    }
+                }
+                if(c==0)
+                x++;
+                word="";
+            }
+        }
+        return x;
+    }
+};
