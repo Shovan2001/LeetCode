25981bdcdb7402a8f131bfc1cb549a216fee1b43
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2024-11-05 23:16:11 +0530

    Time: 1 ms (54.26%) | Memory: 12.2 MB (67.52%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -5,16 +5,16 @@ public:
         int count=0,one=0,zero=0;
         for(int i=1;i<s.length();i=i+2)
         {
-            if(s[i-1]=='1')
-                one++;
-            else if(s[i-1]=='0')
-                zero++;
+            // if(s[i-1]=='1')
+            //     one++;
+            // else if(s[i-1]=='0')
+            //     zero++;
 
             if(s[i]!=s[i-1])
             {
                 count++;
-                one=0;
-                zero=0;
+                // one=0;
+                // zero=0;
             }
         }
         return count;
