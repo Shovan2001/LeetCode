18b28165d46189ba4bc778c390eedff7234a67b8
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2024-11-11 20:55:03 +0530

    Time: 45 ms (10.36%) | Memory: 26.6 MB (97.12%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+bool isPrime(int x)
+    {
+        for(int i=2;i<=x/2;i++)
+        {
+            if(x%i==0)
+            return false;
+        }
+        return true;
+    }
+    bool primeSubOperation(vector<int>& nums) 
+    {
+        for(int i=nums.size()-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            continue;
+            else
+            {
+                for(int p=2;p<nums[i];p++)
+                {
+                    if(isPrime(p))
+                    {
+                        if(nums[i]-p<nums[i+1])
+                        {
+                            nums[i]-=p;
+                            break;
+                        }
+                    }
+                }
+                if(nums[i]>=nums[i+1])
+                return false;
+            }
+        }
+        return true;
+    }
+};
