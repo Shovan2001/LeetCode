616eaed459dd48ab1538fac45c642945c5e7658d
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-01-15 22:10:10 +0530

    Time: 4 ms (12.62%) | Memory: 15.4 MB (22.28%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(vector<int>nums,vector<int> &sub,vector<vector<int>> &ans,int idx)
+    {
+        if(idx==nums.size())
+        {
+            ans.push_back(sub);
+            return;
+        }
+        
+        // vector<int>sub1=sub;
+        // vector<int>sub2=sub;
+        
+        // sub1.push_back(nums[0]);
+        // nums.erase(nums.begin()+0);
+        sub.push_back(nums[idx]);
+        solve(nums,sub,ans,idx+1);
+        sub.pop_back();
+        
+        solve(nums,sub,ans,idx+1);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        vector<vector<int>>ans;
+        vector<int>sub;
+    
+        solve(nums,sub,ans,0);
+        return ans;
+    }
+};
