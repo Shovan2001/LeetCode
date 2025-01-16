e5cb9fb62e0f07f7b29fe758f61c88f4707be834
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-01-16 21:06:17 +0530

    Time: 3 ms (28.77%) | Memory: 12.2 MB (11.86%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -23,30 +23,52 @@ public:
     //     }
     // }
 
-    void solve(vector<vector<int>>&ans,vector<int> &res,vector<int> &nums,int idx)
+    // void solve(vector<vector<int>>&ans,vector<int> &res,vector<int> nums,int idx)
+    // {
+    //     if(idx==nums.size())
+    //     {
+    //         // if(res.size()==nums.size())
+    //         ans.push_back(res);
+    //         return;
+    //     }
+    //     for(int i=idx;i<nums.size();i++)
+    //     {
+    //         res.push_back(nums[i]);
+    //         swap(nums[i],nums[idx]);
+    //         solve(ans,res,nums,idx+1);
+    //         swap(nums[idx],nums[i]);
+    //         res.pop_back();
+    
+    //     }
+    // }
+
+    void solve(vector<vector<int>>&ans,vector<int> &res,vector<int> nums,unordered_set<int> &used,int idx)
     {
-        if(idx==nums.size())
+        if(res.size()==nums.size())
         {
-            // if(res.size()==nums.size())
             ans.push_back(res);
             return;
         }
-        for(int i=idx;i<nums.size();i++)
+
+        for(int i=0;i<nums.size();i++)
         {
-            res.push_back(nums[i]);
-            swap(nums[i],nums[idx]);
-            solve(ans,res,nums,idx+1);
-            swap(nums[idx],nums[i]);
-            res.pop_back();
-    
+            if(used.find(nums[i])==used.end())
+            {
+                used.insert(nums[i]);
+                res.push_back(nums[i]);
+                solve(ans,res,nums,used,idx+1);
+                used.erase(nums[i]);
+                res.pop_back();
+            }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) 
     {    
         vector<vector<int>>ans;
         vector<int>res;
+        unordered_set<int> used;
 
-        solve(ans,res,nums,0);
+        solve(ans,res,nums,used,0);
         return ans;
         // vector<vector<int>>ans;
         // vector<int>perm;
