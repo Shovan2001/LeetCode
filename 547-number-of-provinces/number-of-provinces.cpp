767ea849810deaa1c085c753a1f678902b4214d8
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-04-27 03:01:32 +0530

    Time: 75 ms (5.00%) | Memory: 21.3 MB (11.74%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -11,21 +11,30 @@ public:
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<vector<int>> adj_list(isConnected.size());
+        vector<vector<int>> adj_list(isConnected.size()+1);
+        adj_list.erase(adj_list.begin()+0);
         for (int i = 0; i < isConnected.size(); i++) {
             for (int j = 0; j < isConnected.size(); j++) {
                 if (isConnected[i][j] == 1) {
                     if (i != j) {
-                        adj_list[i].push_back(j);
+                        adj_list[i+1].push_back(j+1);
                     }
                 }
             }
         }
 
+        for(int i=1;i<adj_list.size()+1;i++)
+        {
+            cout<<i<<"--> ";
+            for(int j=0;j<adj_list[i].size();j++)
+            cout<<adj_list[i][j]<<" ";
+            cout<<"\n";
+        }
+
         vector<vector<int>> ans;
         unordered_set<int> found;
 
-        for (int i = 0; i < isConnected.size(); i++) {
+        for (int i = 1; i < isConnected.size()+1; i++) {
             if (found.find(i) == found.end()) {
                 vector<int> component;
                 dfs(adj_list, component, found, i);
