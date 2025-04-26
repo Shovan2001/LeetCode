a143920f60c3ae72c6750086dc0d4f191bb73aa1
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-04-27 02:51:37 +0530

    Time: 79 ms (5.00%) | Memory: 21.3 MB (11.74%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& adj_list, vector<int>& component,
+             unordered_set<int>& found, int node) {
+        component.push_back(node);
+        found.insert(node);
+
+        for (auto it : adj_list[node]) {
+            if (found.find(it) == found.end())
+                dfs(adj_list, component, found, it);
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        vector<vector<int>> adj_list(isConnected.size());
+        for (int i = 0; i < isConnected.size(); i++) {
+            for (int j = 0; j < isConnected.size(); j++) {
+                if (isConnected[i][j] == 1) {
+                    if (i != j) {
+                        adj_list[i].push_back(j);
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<adj_list.size();i++)
+        {
+            cout<<i<<"--> ";
+            for(int j=0;j<adj_list[i].size();j++)
+            cout<<j<<" ";
+            cout<<"\n";
+        }
+
+        vector<vector<int>> ans;
+        unordered_set<int> found;
+
+        for (int i = 0; i < isConnected.size(); i++) {
+            if (found.find(i) == found.end()) {
+                vector<int> component;
+                dfs(adj_list, component, found, i);
+
+                ans.push_back(component);
+            }
+        }
+
+        return ans.size();
+    }
+};
