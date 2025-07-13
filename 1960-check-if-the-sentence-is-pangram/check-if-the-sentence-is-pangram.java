650a1a15f39d39b32fe2ada2e97221abecdc9892
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-07-14 00:45:25 +0530

    Time: 4 ms (25.63%) | Memory: 41.6 MB (79.33%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) 
+    {
+        sentence=sentence.toLowerCase();
+
+        int []alpha=new int[26];
+
+        for(int i=0;i<sentence.length();i++)
+        {
+            alpha[sentence.charAt(i)-97]++;
+        }
+        
+        for(int i=0;i<26;i++)
+        {
+            if(alpha[i]==0)
+            return false;
+        }
+        return true;
+    }
+}
