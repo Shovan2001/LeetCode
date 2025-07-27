6426dc432f08cc6fd65bbab8c32f726b704fddbb
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-07-28 01:46:52 +0530

    Time: 1 ms (24.31%) | Memory: 43.1 MB (26.15%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -5,24 +5,20 @@ class Solution {
         int i=0;
         while(k>0)
         {
-            // System.out.println(currNo);
-            
             if(i>=arr.length)
             {
                 k--;
-                currNo++;
                 i++;
             }
             else if(currNo!=arr[i])
             {
                 k--;
-                currNo++;
             }
             else
             {
-                currNo++;
                 i++;
             }
+            currNo++;
         }
         return currNo-1;
     }
