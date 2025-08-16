600238ad4e652ccc1ba44bc3da20474ecc55403f
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-08-16 13:15:08 +0530

    Time: 50 ms (15.60%) | Memory: 65.4 MB (29.19%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int advBinSearch(int[] potions,long numToFind)
+    {
+        int l=0,h=potions.length;
+
+        while(l<h)
+        {
+            int mid=l+(h-l)/2;
+
+            if(potions[mid]<numToFind)
+            l=mid+1;
+            else
+            h=mid;
+        }
+        return l;
+    }
+    public int[] successfulPairs(int[] spells, int[] potions, long success) 
+    {
+        Arrays.sort(potions);
+        int n=spells.length;
+        int ans[]=new int[n];
+
+        for(int i=0;i<spells.length;i++)
+        {
+            long numToFind;
+
+            if(success%spells[i]!=0)
+                numToFind=(success/spells[i])+1;
+            else
+                numToFind=(success/spells[i]);
+            
+            int pos=advBinSearch(potions,numToFind);
+            
+            if(pos<potions.length)
+            ans[i]=potions.length-pos;
+            else
+            ans[i]=0;
+        }
+        return ans;
+    }
+}
