fc440ef774698b16418382cb3fefe7b5f26c7efc
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-09-04 01:07:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Swap Kth nodes from ends/swap-kth-nodes-from-ends.java
+++ Difficulty: Medium/Swap Kth nodes from ends/swap-kth-nodes-from-ends.java
@@ -12,9 +12,11 @@ class Node {
 class Solution {
     public Node swapKth(Node head, int k) {
         // code here
+        // if head is null OR k is -ve no point in swapping
         if (head == null || k <= 0) 
         return head; 
         
+        //CALCULATE SIZE OF LINKED LIST
         Node temp=head;
         int size=0;
         while(temp!=null)
@@ -23,12 +25,13 @@ class Solution {
             temp=temp.next;
         }
         
+        // if K>SIZE no point in swapping
         if (k > size)
         return head;
         
         
         Node curr=head;
-        // Node prev=null;
+        // LOCATING KTH NODE START
         Node pos1=null;
         int x=0;
         while(curr!=null)
@@ -40,18 +43,16 @@ class Solution {
                 break;
             }
             
-            // prev=curr;
             curr=curr.next;
         }
-        // System.out.println(prev1.data);
-        // System.out.println(pos1.data);
+        
         curr=head;
-        // Node next=null;
+        // LOCATING KTH NODE FROM END
         Node pos2=null;
-        int k1=size-k+1;
+        int k1=size-k;
         while(curr!=null)
         {
-            if(k1==1)
+            if(k1==0)
             {
                 pos2=curr;
                 break;
@@ -59,51 +60,13 @@ class Solution {
             k1--;
             curr=curr.next;
         }
-        // next=pos2.next;
         
-        
+        //SWAP THEIR DATA
         int tempVal=pos1.data;
         pos1.data=pos2.data;
         pos2.data=tempVal;
         
-        
-        // System.out.println(prev.data);
-        // System.out.println(pos1.data);
-        // System.out.println(pos2.data);
-        // System.out.println(next.data);
-        //if(prev!=null)
-        // {
-            // prev.next=pos2;
-            // pos2.next=pos1.next;
-        // }
-        // if(next!=null)
-        // {
-        //     prev.next=next;
-        // }
-        
-        // System.out.println(pos2.data);
-        // System.out.println(next.data);
-        // if(pos1!=null)
-        // pos1.next=pos2;
-        
-        // if(pos2!=null)
-        // pos2.next=pos1;
-        
-        // Node temp2=pos1.next;
-        // pos1.next=pos2.next;
-        // pos2.next=temp2;
-        
-        // if(k==1)
-        // head=pos2;
-        
-        // if(k==size)
-        // head=pos1;
-        
-        // return head;
-        
-        // System.out.println(pos1.data);
-        // System.out.println(pos2.data);
-        
+        // RETURN THE HEAD
         return head;
     }
 }
