660f67a6876a8dc62d00cfe26fd9311fc357ab88
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2025-10-25 10:57:39 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (24.13%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -6,16 +6,17 @@ class Solution {
             int temp=nums[s];
             nums[s]=nums[e];
             nums[e]=temp;
+
             s++;
             e--;
         }
     }
-    public void rotate(int[] nums, int k) 
-    {
-        k=k%nums.length;
-
+    public void rotate(int[] nums, int k) {
+        
+        k%=nums.length;
         reverse(nums,0,nums.length-1);
         reverse(nums,0,k-1);
         reverse(nums,k,nums.length-1);
+
     }
 }
