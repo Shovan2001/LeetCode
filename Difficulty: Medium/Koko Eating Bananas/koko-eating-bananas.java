ff264e4a2c53fc7cc0f4b280efc7cf594e6dd058
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2026-02-10 21:49:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
@@ -1,34 +1,38 @@
 class Solution {
-    public int time_taken(int arr[],int rate)
+    public int hrs_needed_to_finish_with_rate_mid(int[] arr, int x) 
     {
         int time=0;
         for(int i=0;i<arr.length;i++)
         {
-            if(arr[i]%rate!=0)
-            time+=(arr[i]/rate)+1;
-            
+            if(arr[i]%x==0)
+            time+=arr[i]/x;
             else
-            time+=(arr[i]/rate);
+            time+=(arr[i]/x)+1;
         }
-        
         return time;
     }
+    
     public int kokoEat(int[] arr, int k) {
         // code here
-        int l=1,h=Arrays.stream(arr).max().getAsInt();
-                
+        
+        int mini=1;//Arrays.stream(arr).min().getAsInt();
+        int maxi=Arrays.stream(arr).max().getAsInt();
+        
+        // if(k<)
+        
+        int l=mini,h=maxi;
+        
         while(l<h)
         {
             int mid=l+(h-l)/2;
             
-            int hours_taken=time_taken(arr,mid);
+            int hrs_needed=hrs_needed_to_finish_with_rate_mid(arr,mid);
             
-            if(hours_taken>k)
+            if(hrs_needed>k)
             l=mid+1;
             else
             h=mid;
         }
-        
         return l;
     }
 }
