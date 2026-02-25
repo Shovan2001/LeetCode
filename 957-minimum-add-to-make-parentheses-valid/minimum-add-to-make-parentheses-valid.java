6792eaff8b64f7696b344af2b9c2872a3c006bfd
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2026-02-26 00:58:16 +0530

    Time: 1 ms (68.21%) | Memory: 43 MB (37.97%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,22 +1,35 @@
 class Solution {
     public int minAddToMakeValid(String s) 
     {
-        Stack<Character> st=new Stack<>();
+        // Stack<Character> st=new Stack<>();
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     char ch=s.charAt(i);
+        //     if(ch=='(')
+        //     {
+        //         st.push('(');
+        //     }
+        //     else
+        //     {
+        //         if(st.isEmpty() || st.peek()==')')
+        //         st.push(')');
+        //         else
+        //         st.pop();
+        //     }
+        // }
+        // return st.size();
+        int open=0,close=0;
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
             if(ch=='(')
-            {
-                st.push('(');
-            }
+                open++;
             else
-            {
-                if(st.isEmpty() || st.peek()==')')
-                st.push(')');
-                else
-                st.pop();
-            }
+                if(open>0)
+                open--;
+                else // no one can close this bracket
+                close++;
         }
-        return st.size();
+        return open+close;
     }
 }
