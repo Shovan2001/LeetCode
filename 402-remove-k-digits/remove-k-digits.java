6253998390ce09cbbaeab1beff49354350848383
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2026-02-26 03:11:34 +0530

    Time: 98 ms (5.38%) | Memory: 48.5 MB (6.66%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0;i<num.length();i++)
         {
             int num_in_num=num.charAt(i)-'0';
-            // System.out.println(num_in_num);
+            
             while(!st.isEmpty() && num_in_num<st.peek() && k>0)
             {
                 st.pop();
@@ -23,28 +23,13 @@ class Solution {
         }
         if(st.isEmpty())
         return "0";
-        
+
         String ans="";
         while(!st.isEmpty())
         {
-            // System.out.println(st.peek());
             ans=(char)(st.pop()+'0')+ans;
         }
         return ans;
-        // System.out.println(k);
-        
-        // System.out.println(ans);
-        // int pos_1st_elem=-1;
-        // for(int i=0;i<ans.length();i++)
-        // {
-        //     if(ans.charAt(i)!='0')
-        //     {
-        //         pos_1st_elem=i;
-        //         break;
-        //     }
-        // }
-        // if(pos_1st_elem==-1)
-        // return "0";
-        // return ans.substring(pos_1st_elem,ans.length());
+
     }
 }
