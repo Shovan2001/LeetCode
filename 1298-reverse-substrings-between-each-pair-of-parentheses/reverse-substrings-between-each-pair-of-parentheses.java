7495067ed01442ecec01fc635d024cc00c316dad
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2026-03-01 01:24:46 +0530

    Time: 4 ms (55.56%) | Memory: 43.9 MB (65.40%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -4,24 +4,72 @@ class Solution {
         Stack<Integer> st=new Stack<>();
         StringBuilder res=new StringBuilder();
         
-        // if(s.charAt(0)!='(' && s.charAt(s.length()-1)!=')')
-        // {
-        //     s='('+s+')';
-        //     rev=1;
-        // }
         for(int i=0;i<s.length();i++)
         {
-            if(s.charAt(i)=='(')
+            char ch=s.charAt(i);
+
+            if(ch=='(')
+            {
                 st.push(res.length());
-            else if(s.charAt(i)==')')
-            { 
+            }
+            else if(ch==')')
+            {
                 int start_idx=st.pop();
-                String substr_reversed=new StringBuilder(res.substring(start_idx,res.length())).reverse().toString();
-                res.replace(start_idx,res.length(),substr_reversed);
+                String sub_str=res.substring(start_idx,res.length());//returns string not stringbuilder
+                String sub_str_reversed=new StringBuilder(sub_str).reverse().toString();
+                // String sub_str_reversed=res.substring(start_idx,res.length()).reverse().toString();
+                res.replace(start_idx,res.length(),sub_str_reversed);
             }
             else
-                res.append(s.charAt(i));
+            {
+                res.append(ch);
+            }
         }
+
         return res.toString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // // if(s.charAt(0)!='(' && s.charAt(s.length()-1)!=')')
+        // // {
+        // //     s='('+s+')';
+        // //     rev=1;
+        // // }
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(s.charAt(i)=='(')
+        //         st.push(res.length());
+        //     else if(s.charAt(i)==')')
+        //     { 
+        //         int start_idx=st.pop();
+        //         String substr_reversed=new StringBuilder(res.substring(start_idx,res.length())).reverse().toString();
+        //         res.replace(start_idx,res.length(),substr_reversed);
+        //     }
+        //     else
+        //         res.append(s.charAt(i));
+        // }
+        // return res.toString();
     }
 }
