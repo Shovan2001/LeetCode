0243d3230d12d5a00cfedde6305703d4f1c19a61
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2026-03-07 22:29:03 +0530

    Time: 1986 ms (5.38%) | Memory: 57 MB (5.38%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) 
+    {
+        String maxString,minString;
+
+        if(x>y)
+        {
+            maxString="ab";
+            minString="ba";
+        }
+        else
+        {
+            maxString="ba";
+            minString="ab";    
+        }
+        // System.out.println(maxString);
+        // System.out.println(minString);
+        
+        int points=0;
+        Stack<Character>st=new Stack<>();
+
+        //maxString
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+
+            if(ch==maxString.charAt(1) && !st.isEmpty() && st.peek()==maxString.charAt(0))
+            {
+                st.pop();
+                points+=(x>y)?x:y;
+            }
+            else
+            st.push(ch);
+        }
+
+        String str="";
+        while(!st.isEmpty())
+        {
+            str=st.pop()+str;
+        }
+        // System.out.println(str);
+        //minString
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+
+            if(ch==minString.charAt(1) && !st.isEmpty() && st.peek()==minString.charAt(0))
+            {
+                st.pop();
+                points+=(x<y)?x:y;
+                
+            }
+            else
+            st.push(ch);
+        }
+
+        return points;
+    }
+}
