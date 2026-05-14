d38544d5c17aa5003374f160faca44c18a0d2983
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2026-05-14 19:11:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (65.81%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l=0,h=nums.length;
+        
+        while(l<h)
+        {
+            int mid=l+(h-l)/2;
+
+            if(nums[mid]<target)
+            l=mid+1;
+            else
+            h=mid;
+        }
+        return l;
+    }
+}
