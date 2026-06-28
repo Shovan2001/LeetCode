e2518666407f4df7df1979c518485a55578459e3
Shovan Akhuli <83125336+Shovan2001@users.noreply.github.com>
2026-06-29 00:55:40 +0530

    Time: 18 ms (6.04%) | Memory: 46.1 MB (6.25%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -2,11 +2,10 @@ class Solution {
     public int[] frequencySort(int[] nums) 
     {
         Map<Integer,Integer> mp =new HashMap<>();
-        //Integer newNums[]=new Integer[nums.length];
+        
         for(int i=0;i<nums.length;i++)
         {
             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-            // newNums[i]=nums[i];
         }
         
         return Arrays.stream(nums).boxed().sorted((x,y)->{
@@ -14,8 +13,8 @@ class Solution {
             return mp.get(x)-mp.get(y);
             else
             return y-x;
-        }).mapToInt(x->x.intValue()).toArray();
-
+        })//.mapToInt(x->x.intValue()).toArray();
+        .mapToInt(Integer::intValue).toArray();
         // System.out.println(mp);
         // return new int[]{0,0};
     }
